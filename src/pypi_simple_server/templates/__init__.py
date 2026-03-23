b239a25bcc50ee6b6a46c63585f8bdd1fb362849
Sebastian Koslowski <s.koslowski@procitec.de>
2026-03-23 09:31:45 +0100

    client-side relative times (for caching)

--- src/pypi_simple_server/templates/__init__.py
+++ src/pypi_simple_server/templates/__init__.py
@@ -1,20 +1,9 @@
-from datetime import datetime
 from pathlib import Path
 
 import humanize
 from starlette.templating import Jinja2Templates
 
-
-def fromisoformat(date_string: str | None) -> datetime | None:
-    if not date_string:
-        return None
-    dt = datetime.fromisoformat(date_string)
-    return dt.astimezone()
-
-
 _templates = Jinja2Templates(Path(__file__).parent)
-_templates.env.filters["naturaltime"] = humanize.naturaltime
 _templates.env.filters["naturalsize"] = humanize.naturalsize
-_templates.env.filters["fromisoformat"] = fromisoformat
 
 TemplateResponse = _templates.TemplateResponse
